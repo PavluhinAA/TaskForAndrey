421b84cc5dc87e54b50c34f6c8eb14b3c03fa21d
Антон <pavlukhin.ant0n@yandex.ru>
2025-01-14 19:14:02 +0300

    I'm so sorry

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
 type sort interface {
 	quickSort()
-	output()
+	formatting() []string
 }
 
-type byteSlice struct {
-	slice []string
+type stringSlice struct {
+	slice []rune
 }
 
 type floatSlice struct {
@@ -24,142 +22,145 @@ type floatSlice struct {
 }
 
 func main() {
-	inputSlice, err := input()
-	if err != nil {
-		fmt.Println("error reading input")
-		os.Exit(2)
-	}
-	if inputSlice == nil {
-		fmt.Println("No input detected")
-		os.Exit(2)
-	}
-	sliceByte, sliceFloat := typeDefinition(inputSlice)
-	if sliceFloat != nil {
-		sliceFloat.quickSort()
-		sliceFloat.output()
-	}
-	if sliceByte != nil {
-		sliceByte.quickSort()
-		sliceByte.output()
-	}
+
+	inputString, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	inputString = strings.Trim(inputString, "\n")
+	inputSlice := strings.Split(inputString, " ")
+
+	sortingSlice := typeDefinition(inputSlice)
+
+	sortingSlice.quickSort()
+
+	output := sortingSlice.formatting()
+	fmt.Println(strings.Join(output, " "))
+
 }
 
-func input() ([][]byte, error) {
-	sliceByte := make([]byte, 0)
-	finalSlice := make([][]byte, 0)
-	inputSlice := bufio.NewReader(os.Stdin)
-	for {
-		input, err := inputSlice.ReadByte()
-		if err != nil {
-			if err == io.EOF {
-				if len(sliceByte) > 0 {
-					finalSlice = append(finalSlice, sliceByte)
-				}
-				break
-			}
-			return nil, err
-		}
-		if rune(input) == ' ' {
-			copyByte := make([]byte, len(sliceByte))
-			copy(copyByte, sliceByte)
-			finalSlice = append(finalSlice, copyByte)
-			sliceByte = sliceByte[:0]
-			continue
-		} else if rune(input) == '\n' {
-			if len(finalSlice) == 0 {
-				return nil, nil
-			}
-			finalSlice = append(finalSlice, sliceByte)
-			break
-		}
-		sliceByte = append(sliceByte, input)
-	}
-	return finalSlice, nil
-}
+func typeDefinition(slice []string) sort {
 
-func typeDefinition(sliceInterface [][]byte) (sort, sort) {
-	sliceFloat := make([]float64, 0)
-	sliceByte := make([]string, 0)
-	for i := 0; i < len(sliceInterface); i++ {
-		if elementFloat, errFloat := strconv.ParseFloat(string(sliceInterface[i]), 64); errFloat == nil {
-			sliceFloat = append(sliceFloat, elementFloat)
-		} else {
-			sliceByte = append(sliceByte, string(sliceInterface[i]))
+	resultFloat := make([]float64, 0)
+	resultString := make([]rune, 0)
+
+	if _, err := strconv.ParseFloat(slice[0], 64); err == nil {
+
+		for _, element := range slice {
+
+			number, _ := strconv.ParseFloat(element, 64)
+			resultFloat = append(resultFloat, number)
+
 		}
+
+		return &floatSlice{resultFloat}
+
+	} else {
+
+		for _, str := range slice {
+
+			resultString = append(resultString, []rune(str)...)
+
+		}
+
+		return &stringSlice{resultString}
+
 	}
-	return &byteSlice{sliceByte}, &floatSlice{sliceFloat}
+
 }
 
 func (f *floatSlice) quickSort() {
-	if len(f.slice) <= 1 {
+
+	if len(f.slice) <= 0 {
 		return
 	}
-	supportElement := f.slice[0]
-	smallerSlice := 0
-	largestSlice := len(f.slice) - 1
 
-	for smallerSlice <= largestSlice {
-		for f.slice[smallerSlice] < supportElement {
-			smallerSlice++
+	var (
+		smaller, largest []float64
+		supportElement   = f.slice[0]
+	)
+
+	for _, element := range f.slice[1:] {
+
+		if element <= supportElement {
+
+			smaller = append(smaller, element)
+
+		} else {
+
+			largest = append(largest, element)
+
 		}
-		for supportElement < f.slice[largestSlice] {
-			largestSlice--
-		}
-		if smallerSlice <= largestSlice {
-			f.slice[smallerSlice], f.slice[largestSlice] = f.slice[largestSlice], f.slice[smallerSlice]
-			smallerSlice++
-			largestSlice--
-		}
+
 	}
-	if largestSlice > 0 {
-		rightSlice := floatSlice{f.slice[:largestSlice+1]}
-		rightSlice.quickSort()
-	}
-	if smallerSlice < len(f.slice) {
-		leftSlice := floatSlice{f.slice[smallerSlice:]}
-		leftSlice.quickSort()
-	}
+
+	newSmaller := floatSlice{smaller}
+	newLargest := floatSlice{largest}
+
+	newSmaller.quickSort()
+	newLargest.quickSort()
+
+	f.slice = append(append(newSmaller.slice, supportElement), newLargest.slice...)
+
 }
 
-func (b *byteSlice) quickSort() {
-	if len(b.slice) <= 1 {
+func (s *stringSlice) quickSort() {
+
+	if len(s.slice) <= 0 {
 		return
 	}
-	supportElement := b.slice[0]
-	smallerSlice := 0
-	largestSlice := len(b.slice) - 1
 
-	for smallerSlice <= largestSlice {
-		for utf8.RuneCountInString(b.slice[smallerSlice]) < utf8.RuneCountInString(supportElement) {
-			smallerSlice++
+	var (
+		smaller, largest []rune
+		supportElement   = s.slice[0]
+	)
+
+	for _, element := range s.slice[1:] {
+
+		if element <= supportElement {
+
+			smaller = append(smaller, element)
+
+		} else {
+
+			largest = append(largest, element)
+
 		}
-		for utf8.RuneCountInString(supportElement) < utf8.RuneCountInString(b.slice[largestSlice]) {
-			largestSlice--
-		}
-		if smallerSlice <= largestSlice {
-			b.slice[smallerSlice], b.slice[largestSlice] = b.slice[largestSlice], b.slice[smallerSlice]
-			smallerSlice++
-			largestSlice--
-		}
+
 	}
-	if largestSlice > 0 {
-		rightSlice := byteSlice{b.slice[:largestSlice+1]}
-		rightSlice.quickSort()
-	}
-	if smallerSlice < len(b.slice) {
-		leftSlice := byteSlice{b.slice[smallerSlice:]}
-		leftSlice.quickSort()
-	}
+
+	newSmaller := stringSlice{smaller}
+	newLargest := stringSlice{largest}
+
+	newSmaller.quickSort()
+	newLargest.quickSort()
+
+	s.slice = append(append(newSmaller.slice, supportElement), newLargest.slice...)
+
 }
 
-func (f *floatSlice) output() {
-	outputSlice := make([]string, len(f.slice))
-	for j, number := range f.slice {
-		outputSlice[j] = strconv.FormatFloat(number, 'g', 10, 64)
+func (f *floatSlice) formatting() []string {
+
+	outputSlice := make([]string, 0)
+
+	for i, number := range f.slice {
+
+		formatNumber := strconv.FormatFloat(number, 'g', 10, 64)
+		outputSlice[i] = formatNumber
+
 	}
-	fmt.Println(strings.Join(outputSlice, " "))
+
+	return outputSlice
+
 }
 
-func (b *byteSlice) output() {
-	fmt.Println(strings.Join(b.slice, " "))
+func (s *stringSlice) formatting() []string {
+
+	outputSlice := make([]string, 0)
+
+	for i, element := range s.slice {
+
+		outputSlice[i] = string(element)
+
+	}
+
+	return outputSlice
+
 }
