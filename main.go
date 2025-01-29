91929ec6e0fdf085ed1bfd2408e72f8986cd2466
Антон <pavlukhin.ant0n@yandex.ru>
2025-01-30 01:11:14 +0300

    Random number

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	var (
 		sum int32
 	)
+
 	stop := make(chan struct{})
 	wg := new(sync.WaitGroup)
 
@@ -50,6 +51,7 @@ func main() {
 	}()
 
 	for sum != 100 {
+
 		if sum > 100 {
 			fmt.Println(sum)
 			sum = 0
@@ -57,93 +59,3 @@ func main() {
 	}
 	wg.Wait()
 }
-
-/*package main
-
-import (
-	"fmt"
-	"math/rand"
-	"sync"
-	"time"
-)
-
-const N = 20
-
-func main() {
-
-	fn := func(x int) int {
-		time.Sleep(time.Duration(rand.Int31n(N)) * time.Second)
-		return x * 2
-	}
-	in1 := make(chan int, N)
-	in2 := make(chan int, N)
-	out := make(chan int, N)
-
-	start := time.Now()
-	merge2Channels(fn, in1, in2, out, N+1)
-	for i := 0; i < N+1; i++ {
-		in1 <- i
-		in2 <- i
-	}
-
-	orderFail := false
-	EvenFail := false
-	for i, prev := 0, 0; i < N; i++ {
-		c := <-out
-		if c%2 != 0 {
-			EvenFail = true
-		}
-		if prev >= c && i != 0 {
-			orderFail = true
-		}
-		prev = c
-		fmt.Println(c)
-	}
-	if orderFail {
-		fmt.Println("порядок нарушен")
-	}
-	if EvenFail {
-		fmt.Println("Есть не четные")
-	}
-	duration := time.Since(start)
-	if duration.Seconds() > N {
-		fmt.Println("Время превышено")
-	}
-	fmt.Println("Время выполнения: ", duration)
-}
-
-func merge2Channels(fn func(int) int, in1 chan int, in2 <-chan int, out chan<- int, n int) {
-
-	FX1 := func(x1 int, chan1 chan<- int, wg *sync.WaitGroup) {
-		defer close(chan1)
-		defer wg.Done()
-		chan1 <- fn(x1)
-	}
-
-	FX2 := func(x2 int, chan2 chan<- int, wg *sync.WaitGroup) {
-		defer close(chan2)
-		defer wg.Done()
-		chan2 <- fn(x2)
-	}
-
-	funC := func() {
-		wg := new(sync.WaitGroup)
-		defer close(out)
-
-		for i := 0; i < n; i++ {
-			chan1 := make(chan int)
-			chan2 := make(chan int)
-			x1, x2 := <-in1, <-in2
-			wg.Add(2)
-			go FX1(x1, chan1, wg)
-			go FX2(x2, chan2, wg)
-
-			y1, y2 := <-chan1, <-chan2
-			res := y1 + y2
-			out <- res
-
-		}
-		wg.Wait()
-	}
-	go funC()
-}*/
