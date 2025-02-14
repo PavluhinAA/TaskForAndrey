ab3c1225609740423b46c3fefdf221de4e6cd3c7
Антон <pavlukhin.ant0n@yandex.ru>
2025-02-14 17:07:29 +0300

    Random number

--- main.go
+++ main.go
@@ -8,55 +8,50 @@ import (
 	"time"
 )
 
-func main() {
-	var (
-		sum int32
-	)
+func randomNumber(wg *sync.WaitGroup, sum *atomic.Int32, stop chan struct{}) {
+	wg.Add(1)
+	defer wg.Done()
+	tick := time.NewTicker(time.Second * 2)
+	defer tick.Stop()
 
-	stop := make(chan struct{})
-	wg := new(sync.WaitGroup)
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		tick := time.NewTicker(time.Second)
-		defer tick.Stop()
-
-		for {
-			select {
-			case <-tick.C:
-				atomic.AddInt32(&sum, rand.Int31n(101))
-			case <-stop:
-				return
-			}
-		}
-
-	}()
-
-	go func() {
-		defer wg.Done()
-		tick := time.NewTicker(time.Second * 5)
-		defer tick.Stop()
-
-		for {
-			select {
-			case <-tick.C:
-				atomic.AddInt32(&sum, rand.Int31n(11))
-			case <-stop:
-				return
-			}
-		}
-
-	}()
-
-	for sum != 100 {
-
-		if sum > 100 {
-			fmt.Println(sum)
-			sum = 0
+	for {
+		select {
+		case <-tick.C:
+			sum.Add(rand.Int31n(101))
+		case <-stop:
+			return
 		}
 	}
-	close(stop)
+}
+
+func check(wg *sync.WaitGroup, sum *atomic.Int32, stop chan struct{}) {
+	wg.Add(1)
+	defer wg.Done()
+
+	for sum.Load() != 100 {
+
+		if sum.Load() > 100 {
+			fmt.Println(sum.Load())
+			sum.Store(0)
+		}
+	}
+	stop <- struct{}{}
+}
+func shutdown(wg *sync.WaitGroup, sum *atomic.Int32, stop chan struct{}) {
+
+}
+func main() {
+	var (
+		sum  atomic.Int32
+		wg   sync.WaitGroup
+		stop = make(chan struct{})
+	)
+
+	go randomNumber(&wg, &sum, stop)
+	go randomNumber(&wg, &sum, stop)
+	go check(&wg, &sum, stop)
+	go shutdown(&wg, &sum, stop)
+
 	wg.Wait()
+	close(stop)
 }
