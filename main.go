32cd9c246304082cd111eac6aa96429e8aa8a89d
Антон <pavlukhin.ant0n@yandex.ru>
2025-02-27 01:43:48 +0300

    Random number

--- main.go
+++ main.go
@@ -11,43 +11,52 @@ import (
 	"time"
 )
 
-func randomNumber(wg *sync.WaitGroup, sum *atomic.Int32, stop chan struct{}) {
+func randomNumber(wg *sync.WaitGroup, num *atomic.Int32, stop chan struct{}, seed int64) {
 	wg.Add(1)
 	defer wg.Done()
-
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	tick := time.NewTicker(time.Second * 2)
 	defer tick.Stop()
 
+	random := rand.New(rand.NewSource(seed))
+
 	for {
 		select {
-
 		case <-tick.C:
-			sum.Add(int32(random.Intn(101)))
-
+			num.Add(int32(random.Intn(101)))
+			fmt.Println("number:", num.Load())
 		case <-stop:
+			fmt.Println("stopping")
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 			return
 		}
 	}
 }
 
-func check(wg *sync.WaitGroup, sum, sig *atomic.Int32, stop chan struct{}) {
+func check(wg *sync.WaitGroup, num1, num2, sig *atomic.Int32, stop chan struct{}) {
 	wg.Add(1)
 	defer wg.Done()
 
-	for sum.Load() != 100 {
-		time.Sleep(time.Second * 2)
+	for {
 
 		if sig.Load() == 1 {
-			wg.Done()
 			return
 		}
-		if sum.Load() > 100 {
 
-			fmt.Println(sum.Load())
+		if num1.Load() != 0 && num2.Load() != 0 {
+			sum := num1.Load() + num2.Load()
+			num1.Store(0)
+			num2.Store(0)
+			if sum == 100 {
+				break
+			}
+
+			if sum > 100 {
+
+				fmt.Println(sum)
+
+			}
 		}
-		sum.Store(0)
 	}
 	stop <- struct{}{}
 	fmt.Println("the process is completed because the amount is 100")
@@ -59,7 +68,7 @@ func shutdown(wg *sync.WaitGroup, sig *atomic.Int32, stop chan struct{}) {
 	defer wg.Done()
 
 	var stopSignal = make(chan os.Signal, 1)
-	signal.Notify(stopSignal, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGKILL)
+	signal.Notify(stopSignal, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 
 	select {
 
@@ -70,19 +79,23 @@ func shutdown(wg *sync.WaitGroup, sig *atomic.Int32, stop chan struct{}) {
 		return
 
 	case <-stop:
+		fmt.Println("stopShutdown")
 		return
 	}
 }
+
 func main() {
 	var (
-		sum, sig atomic.Int32
-		wg       sync.WaitGroup
-		stop     = make(chan struct{})
+		num1, num2, sig atomic.Int32
+		wg              sync.WaitGroup
+		stop            = make(chan struct{})
+		seed1           = time.Now().UnixNano()
+		seed2           = time.Now().UnixNano() * time.Now().UnixNano()
 	)
 
-	go randomNumber(&wg, &sum, stop)
-	go randomNumber(&wg, &sum, stop)
-	go check(&wg, &sum, &sig, stop)
+	go randomNumber(&wg, &num1, stop, seed1)
+	go randomNumber(&wg, &num2, stop, seed2)
+	go check(&wg, &num1, &num2, &sig, stop)
 	go shutdown(&wg, &sig, stop)
 
 	wg.Wait()
